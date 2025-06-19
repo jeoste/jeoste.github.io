77e00f0c8be4a77fca52cac47fee55c51dcc58ec
Jeoffrey Stéphan <jeoffrey.stephan.pro@gmail.com>
2025-06-20 01:59:48 +0900

    Update site configuration: enable archives and adjust formatting for clarity

--- src/config.ts
+++ src/config.ts
@@ -8,11 +8,9 @@ export const SITE = {
   //ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerIndex: 4,
-  postPerPage: 4,
-  scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
-  showArchives: false,
+  postPerPage: 4,  scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
+  showArchives: true,
   showBackButton: true, // show back button in post detail
-  dynamicOgImage: true,
-  lang: "en", // html lang code. Set this empty and default will be "en"
+  dynamicOgImage: true,  lang: "en", // html lang code. Set this empty and default will be "en"
   timezone: "Asia/Seoul", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 } as const;
